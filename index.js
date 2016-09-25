2990310631152a91f36c0c471a1110d32c84067f
detailyang <detailyang@gmail.com>
2016-09-25 15:18:33 +0800

    add binary command for nodejs

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+#! /usr/bin/env node
 /*
 * @Author: detailyang
 * @Date:   2016-09-25 15:00:17
 * @Last Modified by:   detailyang
-* @Last Modified time: 2016-09-25 15:00:35
+* @Last Modified time: 2016-09-25 15:18:24
 */
 
 'use strict';
