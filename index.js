6b0f06474b14a8d3698248ef7d107bdd404a7fa7
detailyang <detailyang@gmail.com>
2016-09-25 15:00:49 +0800

    as node modules

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+* @Author: detailyang
+* @Date:   2016-09-25 15:00:17
+* @Last Modified by:   detailyang
+* @Last Modified time: 2016-09-25 15:00:35
+*/
+
+'use strict';
+
+
+console.log('https://detailyang.github.io');
