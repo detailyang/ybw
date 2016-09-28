a2ec7d729db989cd41b78be5280dddc235b39e1b
detailyang <detailyang@gmail.com>
2016-09-28 09:46:55 +0800

    add go

---
+++ index.go
@@ -0,0 +1,14 @@
+/*
+* @Author: detailyang
+* @Date:   2016-09-28 09:45:28
+* @Last Modified by:   detailyang
+* @Last Modified time: 2016-09-28 09:45:42
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("I'm at https://github.com/detailyang")
+}
